9238b1645da83c7d57052ae45613485aa3bc89c4
Andrey Repin <anrdaemon@yandex.ru>
2018-03-25 16:17:22 +0000

    * Move and update bootstrap autoloader.

---
+++ Minecraft/classloader.php
@@ -0,0 +1,31 @@
+<?php
+/** Universal stackable classloader.
+*
+* @version SVN: $Id$
+*/
+
+namespace AnrDaemon\Minecraft;
+
+return \call_user_func(function()
+{
+  $nsl = \strlen(__NAMESPACE__);
+  return \spl_autoload_register(
+    function($className)
+    use($nsl)
+    {
+      if(\strncmp($className, __NAMESPACE__, $nsl) !== 0)
+        return;
+
+      $className = \substr($className, $nsl);
+      if(\strlen($className) < 2)
+        return;
+
+      $path = \realpath(__DIR__ . \strtr("$className.php", '\\', '/'));
+      if(!empty($path))
+      {
+        return include_once $path;
+      }
+    }
+  );
+}
+);
